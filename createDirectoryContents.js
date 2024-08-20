16e34c139e513427175c0b159f1f616f258b32f5
GauravSamanta <gauravsamanta143@gmail.com>
2024-08-20 11:31:46 +0530

    proto-1

--- createDirectoryContents.js
+++ createDirectoryContents.js
@@ -13,9 +13,6 @@ const createDirectoryContents = (templatePath, newProjectPath) => {
     if (stats.isFile()) {
       const contents = fs.readFileSync(origFilePath, 'utf8');
 
-      // Rename
-      if (file === '.npmignore') file = '.gitignore';
-
       const writePath = `${CURR_DIR}/${newProjectPath}/${file}`;
       fs.writeFileSync(writePath, contents, 'utf8');
     } else if (stats.isDirectory()) {
